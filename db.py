18b5a4edcf91ffac39633af4cea87b26be8aa6bb
Koizito <dspereira93@hotmail.com>
2026-09-23 23:02:27 +0100

    Implemented insert to database for the received values

--- db.py
+++ db.py
@@ -1,12 +1,7 @@
-import psycopg
-
-DATABASE_URL = "postgresql://app:app@localhost:5432/app"
-
-def insert_readings(readings):
-    with psycopg.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.executemany(
-                "INSERT INTO readings (time, value, tags) VALUES (%s, %s, %s)",
-                readings,
-            )
-        conn.commit()
+def insert_reading(conn, reading):
+    with conn.cursor() as cur:
+        cur.execute(
+            "INSERT INTO readings (time, value, tags) VALUES (%s, %s, %s)",
+            reading,
+        )
+    conn.commit()
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import time
 import requests
 import struct
+from db import insert_reading
+import psycopg
+from datetime import datetime, timezone
 
+DATABASE_URL = "postgresql://app:app@localhost:5432/app"
 URL = "http://localhost:28462/"
-INTERVAL = 0.01
+INTERVAL = 1
 
 def main():
+    conn = psycopg.connect(DATABASE_URL)
     while True:
         try:
             response = requests.get(URL)
@@ -14,6 +19,11 @@ def main():
             print(json_response["tags"])
             print(json_response["value"])
             print(struct.unpack('<f', bytes(json_response["value"]))[0])
+
+            value = struct.unpack("<f", bytes(json_response["value"]))[0]
+            timestamp = ts = datetime.fromtimestamp(json_response["time"], tz=timezone.utc)
+
+            insert_reading(conn, (timestamp, value, json_response["tags"]))
         except requests.RequestException:
             pass
         time.sleep(INTERVAL)
